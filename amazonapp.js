c510494172c42ed6c529f247a1793f90ee786562
grambo <gramham@live.com>
2020-03-16 10:34:08 -0600

    got inquirer and mysql hooked up and added all relevant files

---
+++ amazonapp.js
@@ -0,0 +1,33 @@
+const inquirer = require('inquirer'); 
+const mysql = require("mysql");
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
+});
+
+function afterConnection(){
+    console.log("hi");
+}
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
